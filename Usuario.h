b01e193e0e59cdf191269f298c9123969bca33fc
Gustavo Deves Bueno <gdevesbueno02@gmail.com>
2025-10-08 15:06:32 -0300

    Add files via upload

---
+++ Usuario.h
@@ -0,0 +1,27 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <string>
+#include <map>
+
+class Usuario {
+public:
+    std::string username;
+    std::string password;
+    // colecao: mapa idFigurinha -> status (0=na colecao,1=colada,2=disponivel para troca)
+    std::map<int,int> colecao;
+
+    Usuario();
+    Usuario(const std::string &u, const std::string &p);
+
+    bool possui(int id) const;
+    int status(int id) const;
+    void adicionar(int id, int status);
+    void remover(int id);
+    void colar(int id);
+    void disponibilizarParaTroca(int id);
+    std::string colecaoToString() const;
+    void carregarColecaoFromString(const std::string &s);
+};
+
+#endif
